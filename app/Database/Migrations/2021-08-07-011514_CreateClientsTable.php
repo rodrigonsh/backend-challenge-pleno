0bfdf82fc0cdd556434f967c0d151de764dbd93b
Rodrigo Nishino <rodrigo.nsh@gmail.com>
2021-08-06 21:25:00 -0400

    users, services & clients migrations

---
+++ app/Database/Migrations/2021-08-07-011514_CreateClientsTable.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateClientsTable extends Migration
+{
+	public function up()
+	{
+		
+		$this->forge->addField([
+
+			'id' => 
+			[
+				'type'           => 'INT',
+				'constraint'     => 5,
+				'unsigned'       => true,
+				'auto_increment' => true,
+			],
+			
+			'user_id' => 
+			[
+				'type'           => 'INT',
+				'constraint'     => 5,
+				'unsigned'       => true,
+			],
+
+			'title' =>
+			[
+				'type'       => 'VARCHAR',
+				'constraint' => '100',
+			],
+
+			'image' =>
+			[
+				'type'       => 'VARCHAR',
+				'constraint' => '100',
+			],
+			
+			'link' =>
+			[
+				'type'       => 'VARCHAR',
+				'constraint' => '100',
+			],
+
+			'exibir' =>
+			[
+				'type'       => 'INT',
+				'constraint' => '1',
+			],
+
+
+		]);
+
+		$this->forge->addKey('id', true);
+
+		$this->forge->addForeignKey('user_id','users','id');
+
+		$this->forge->createTable('clients');
+
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('clients');
+	}
+}
